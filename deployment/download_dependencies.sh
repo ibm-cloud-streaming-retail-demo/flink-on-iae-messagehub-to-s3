6c679f398db38c9753a5ad2645b7eaf6954aafc2
Chris Snow <chsnow123@gmail.com>
2018-02-07 18:38:48 +0000

    Update download_dependencies.sh

--- deployment/download_dependencies.sh
+++ deployment/download_dependencies.sh
@@ -23,17 +23,19 @@ cat << EOF > ivysettings.xml
 </ivysettings>
 EOF
 
-java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency org.apache.hadoop hadoop-aws 2.7.3
-java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.amazonaws aws-java-sdk-s3 1.11.183
-java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.amazonaws aws-java-sdk-core 1.11.183
-java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.amazonaws aws-java-sdk-kms 1.11.183
-java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.fasterxml.jackson.core jackson-annotations 2.6.7
-java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.fasterxml.jackson.core jackson-core 2.6.7
-java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.fasterxml.jackson.core jackson-databind 2.6.7
-java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency joda-time joda-time 2.8.1
-java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency org.apache.httpcomponents httpcore 4.4.4
-java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency org.apache.httpcomponents httpclient 4.5.3
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -notransitive -dependency org.apache.hadoop hadoop-aws 2.7.3
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -notransitive -dependency com.amazonaws aws-java-sdk-s3 1.11.183
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -notransitive -dependency com.amazonaws aws-java-sdk-core 1.11.183
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -notransitive -dependency com.amazonaws aws-java-sdk-kms 1.11.183
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -notransitive -dependency com.fasterxml.jackson.core jackson-annotations 2.6.7
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -notransitive -dependency com.fasterxml.jackson.core jackson-core 2.6.7
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -notransitive -dependency com.fasterxml.jackson.core jackson-databind 2.6.7
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -notransitive -dependency joda-time joda-time 2.8.1
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -notransitive -dependency org.apache.httpcomponents httpcore 4.4.4
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -notransitive -dependency org.apache.httpcomponents httpclient 4.5.3
 
+find $TMPDIR -name *-javadoc.jar | xargs rm
+find $TMPDIR -name *-sources.jar | xargs rm
 find $TMPDIR -name *.jar | xargs cp -t $DESTDIR
 
 rm -rf $TMPDIR
