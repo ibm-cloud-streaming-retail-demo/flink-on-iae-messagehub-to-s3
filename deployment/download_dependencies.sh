f8ccab1a20ee158a67c4dc6f8ed5a7e2cfa698be
Chris Snow <chsnow123@gmail.com>
2018-02-07 17:53:10 +0000

    Create download_dependencies.sh

---
+++ deployment/download_dependencies.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+set -e
+set -u
+
+DESTDIR=/home/wce/clsadmin/flink-on-iae-messagehub-to-s3/deployment/flink-1.4.0/lib/
+TMPDIR=$(mktemp -d)
+
+if [ ! -d apache-ivy-2.4.0 ]; then
+   wget -q -c http://apache.mirror.anlx.net//ant/ivy/2.4.0/apache-ivy-2.4.0-bin.zip
+   unzip apache-ivy-2.4.0-bin.zip
+fi
+
+cat << EOF > ivysettings.xml
+<ivysettings>
+    <settings defaultResolver="chain"/>
+    <caches  defaultCacheDir="${TMPDIR}" />
+    <resolvers>
+        <chain name="chain">
+            <ibiblio name="central" m2compatible="true"/>
+        </chain>
+    </resolvers>
+</ivysettings>
+EOF
+
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency org.apache.hadoop hadoop-aws 2.7.3
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.amazonaws aws-java-sdk-s3 1.11.183
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.amazonaws aws-java-sdk-core 1.11.183
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.amazonaws aws-java-sdk-kms 1.11.183
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.fasterxml.jackson.core jackson-annotations 2.6.7
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.fasterxml.jackson.core jackson-core 2.6.7
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency com.fasterxml.jackson.core jackson-databind 2.6.7
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency joda-time joda-time 2.8.1
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency org.apache.httpcomponents httpcore 4.4.4
+java -jar apache-ivy-2.4.0/ivy-2.4.0.jar -settings ivysettings.xml -dependency org.apache.httpcomponents httpclient 4.5.3
+
+find $TMPDIR -name *.jar | xargs cp -t $DESTDIR
+
+rm -rf $TMPDIR
