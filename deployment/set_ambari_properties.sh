527cf0fe7f20a9f0a0759c82376f85e57473cbab
Chris Snow <snowch@localhost.localdomain>
2018-02-07 16:06:08 +0000

    enable ambari check

--- deployment/set_ambari_properties.sh
+++ deployment/set_ambari_properties.sh
@@ -35,16 +35,19 @@ set_key "fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem"
 CLUSTER_NAME=AnalyticsEngine
 
 curl -k -v --user $IAE_USERNAME:$IAE_PASSWORD -H "X-Requested-By: ambari" -i -X PUT -d '{"RequestInfo": {"context": "Stop All Services via REST"}, "ServiceInfo": {"state":"INSTALLED"}}' https://$IAE_AMBARI_HOSTNAME:$IAE_AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/services
+
+    # FIXME: enable more sophisticated checks for service status
     sleep 200
 
 curl -k -v --user $IAE_USERNAME:$IAE_PASSWORD -H "X-Requested-By: ambari" -i -X PUT -d '{"RequestInfo": {"context": "Start All Services via REST"}, "ServiceInfo":{"state":"STARTED"}}' https://$IAE_AMBARI_HOSTNAME:$IAE_AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/services
+
+    # FIXME: enable more sophisticated checks for service status
     sleep 700
 
 
-# FIXME: enable more sophisticated checks for service status
-#python ./verify_ambari_services.py \
-#	$IAE_AMBARI_HOSTNAME \
-#	$AMBARI_PORT \
-#	$IAE_USERNAME \
-#	$IAE_PASSWORD \
-#	$CLUSTER_NAME
+python ./verify_ambari_services.py \
+	$IAE_AMBARI_HOSTNAME \
+	$IAE_AMBARI_PORT \
+	$IAE_USERNAME \
+	$IAE_PASSWORD \
+	$CLUSTER_NAME
