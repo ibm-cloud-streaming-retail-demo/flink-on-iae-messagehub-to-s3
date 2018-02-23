d82795618713193178d2a20fe9cab67fcdd4ff65
Chris Snow <chsnow123@gmail.com>
2018-02-23 08:45:56 +0000

    Update verify_ambari_services.py

--- deployment/verify_ambari_services.py
+++ deployment/verify_ambari_services.py
@@ -4,12 +4,13 @@ import os
 import sys
 import json
 import requests
+import time
 from datetime import datetime, timedelta
 
 if len(sys.argv) != 6:
    print("Usage: %s AMBARI_HOSTNAME AMBARI_PORT AMBARI_USERNAME AMBARI_PASSWORD CLUSTER_NAME", sys.argv[0])
    exit(-1)
-   
+
 AMBARI_HOSTNAME = sys.argv[1]
 AMBARI_PORT = sys.argv[2]
 AMBARI_USERNAME = sys.argv[3]
@@ -24,10 +25,18 @@ response.raise_for_status()
 
 max_time = datetime.now() + timedelta(minutes=30)
 
-while True:
+all_required_services_are_started = False
+
+while not all_required_services_are_started:
     if datetime.now() > max_time:
        println('Unable to verify all services started within 30 minutes')
        sys.exit(-1)
+
+    all_required_services_are_started = True
+
+    print('Getting service status')
+    response = requests.get( url, auth=auth )
+    response.raise_for_status()
 
     for svc in response.json()['items']:
         response = requests.get( svc['href'], auth=auth )
@@ -38,9 +47,12 @@ while True:
         state = j['state']
 
         if service_name not in ['SQOOP', 'SLIDER', 'TEZ', 'PIG']:
-            print(service_name + ' ' + state)
+            print(str(datetime.now()) + ' ' + service_name + ' ' + state)
             if state != 'STARTED':
-                continue
+	        all_required_services_are_started = False
 
-    print('All services started as expected')
-    sys.exit(0) # success!
+    print('sleeping 30 seconds')
+    time.sleep(30)
+
+print('All services started as expected')
+sys.exit(0) # success!
