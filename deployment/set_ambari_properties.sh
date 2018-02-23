42aa0fc770925be5ca1ac1d6d24478de5b2afac6
Chris Snow <chsnow123@gmail.com>
2018-02-23 08:26:29 +0000

    Update set_ambari_properties.sh

--- deployment/set_ambari_properties.sh
+++ deployment/set_ambari_properties.sh
@@ -34,16 +34,9 @@ set_key "fs.s3.impl" "org.apache.hadoop.fs.s3a.S3AFileSystem"
 
 CLUSTER_NAME=AnalyticsEngine
 
-curl -k -v --user $IAE_USERNAME:$IAE_PASSWORD -H "X-Requested-By: ambari" -i -X PUT -d '{"RequestInfo": {"context": "Stop All Services via REST"}, "ServiceInfo": {"state":"INSTALLED"}}' https://$IAE_AMBARI_HOSTNAME:$IAE_AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/services
-
-    # FIXME: enable more sophisticated checks for service status
-    sleep 200
-
-curl -k -v --user $IAE_USERNAME:$IAE_PASSWORD -H "X-Requested-By: ambari" -i -X PUT -d '{"RequestInfo": {"context": "Start All Services via REST"}, "ServiceInfo":{"state":"STARTED"}}' https://$IAE_AMBARI_HOSTNAME:$IAE_AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/services
-
-    # FIXME: enable more sophisticated checks for service status
-    sleep 700
-
+curl -v --user $IAE_USERNAME:$IAE_PASSWORD -H "X-Requested-By: ambari" -i -X POST \
+     -d '{ "RequestInfo": {"command": "RESTART","context": "Restart all required services","operation_level": "host_component"}, "Requests/resource_filters": [{"hosts_predicate": "HostRoles/stale_configs=true" }] }' \
+     https://$IAE_AMBARI_HOSTNAME:$IAE_AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/requests
 
 python ./verify_ambari_services.py \
 	$IAE_AMBARI_HOSTNAME \
